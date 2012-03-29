5af907fea4e49d4147cb9759b940b44eae65372d
Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>
2012-03-29 11:57:32 +0300

    Visual studio test
    Signed-off-by: Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>

---
+++ IssueReportManagementTest/IssueReportManagementTest/Controllers/ActivityController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IssueReportManagementTest.Models;
+
+namespace IssueReportManagementTest.Controllers
+{ 
+    public class ActivityController : Controller
+    {
+        private IssueContext db = new IssueContext();
+
+        //
+        // GET: /Activity/
+
+        public ViewResult Index()
+        {
+            var activities = db.Activities.Include(a => a.Issue);
+            return View(activities.ToList());
+        }
+
+        //
+        // GET: /Activity/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Activity activity = db.Activities.Find(id);
+            return View(activity);
+        }
+
+        //
+        // GET: /Activity/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.IssueID = new SelectList(db.Issues, "IssueID", "Title");
+            return View();
+        } 
+
+        //
+        // POST: /Activity/Create
+
+        [HttpPost]
+        public ActionResult Create(Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Activities.Add(activity);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.IssueID = new SelectList(db.Issues, "IssueID", "Title", activity.IssueID);
+            return View(activity);
+        }
+        
+        //
+        // GET: /Activity/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Activity activity = db.Activities.Find(id);
+            ViewBag.IssueID = new SelectList(db.Issues, "IssueID", "Title", activity.IssueID);
+            return View(activity);
+        }
+
+        //
+        // POST: /Activity/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Activity activity)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(activity).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IssueID = new SelectList(db.Issues, "IssueID", "Title", activity.IssueID);
+            return View(activity);
+        }
+
+        //
+        // GET: /Activity/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Activity activity = db.Activities.Find(id);
+            return View(activity);
+        }
+
+        //
+        // POST: /Activity/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Activity activity = db.Activities.Find(id);
+            db.Activities.Remove(activity);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IssueReportManagementTest/IssueReportManagementTest/Models/Activity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace IssueReportManagementTest.Models
+{
+    public class Activity
+    {
+        [Key]
+        public int ActivityID { get; set; }
+        //Added automatic
+        public DateTime Added { get; set; }
+        //Added automatic
+        public string Employee { get; set; }
+        //What has employee done
+        public string Context { get; set; }
+        //What issue is this activity using
+        
+        public virtual Issue Issue { get; set; }
+        public IEnumerable<Issue> Issues { get; set; }
+        [ForeignKey("Issue")]
+        public int IssueID { get; set; }
+        
+
+    }
+}

780e92ff07a8a4c71626ccf69b7c2ed43bf69a68
Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>
2012-04-13 14:43:01 +0300

    IssueListViewModel.cs wasn't included
    Signed-off-by: Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>

---
+++ IssueReportManagementTest/IssueReportManagementTest/ViewModel/IssueListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IssueReportManagementTest.Models;
+
+namespace IssueReportManagementTest.ViewModel
+{
+    public class IssueListViewModel
+    {
+        public IEnumerable<Issue> lissue { get; set; }
+    }
+}

12463e6ceb5766dcfad4b5fbaef4d17d498e6d1a
unknown <Iiro@Iiro-LAPTOP.(none)>
2012-04-24 12:06:05 +0300

    Errorit nätisti

---
+++ IssueReportManagementTest/IssueReportManagementTest/Views/ViewModel/IssueViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IssueReportManagementTest.Models;
+
+namespace IssueReportManagementTest.ViewModel
+{
+    public class IssueViewModel
+    {
+
+        public Issue cissue { get; set; }
+        public IEnumerable<Activity> cactivities { get; set; }
+        public string cstate { get; set; }
+    }
+}

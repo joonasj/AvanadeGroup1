b0ce55f6cfcead6ec575a1940717b2a20207ce92
Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>
2012-05-03 14:54:00 +0300

    ReportIssueViewModel.cs added
    Signed-off-by: Joonas Jauhiainen <joonas.jauhiainen@metropolia.fi>

---
+++ IssueReportManagementTest/IssueReportManagementTest/ViewModel/ReportIssueViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IssueReportManagementTest.Models;
+using IssueReportManagementTest.ViewModel;
+namespace IssueReportManagementTest.ViewModel
+{
+    public class ReportIssueViewModel
+    {
+        public IEnumerable<IssueViewModel> report_issues {get; set;}
+
+    }
+}
